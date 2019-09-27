a04e0c22208a48d133b965d86977e09e50c4e6d8
son_hoang_ <sonhoang2610@gmail.com>
2019-09-27 09:13:05 +0700

    no message

---
+++ Assets/SpaceJourney/Scripts/AudioPlay.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour {
+    [SerializeField]
+    AudioClip[] audios;
+    [SerializeField]
+    Pos randomAudio = new Pos(0,0);
+    [SerializeField]
+    AudioSource backGroundMusic = null;
+    [SerializeField]
+    bool playOnAwake = true;
+    [SerializeField]
+    bool isLoop = true;
+    [SerializeField]
+    float delay = 0;
+
+    public void pauseBackGroundMusic()
+    {
+        backGroundMusic.Pause();
+    }
+    int enable = 0;
+    private void Awake()
+    {
+        //if (playOnAwake)
+        //{
+        //    if (!backGroundMusic)
+        //    {
+        //        SoundManager.Instance.PlaySound(audio, Vector3.zero);
+        //    }
+        //    else
+        //    {
+        //        SoundManager.Instance.PlayBackgroundMusic(backGroundMusic);
+        //    }
+        //}
+    }
+
+    public IEnumerator delayAction(float pSec, System.Action pAction)
+    {
+        yield return new WaitForSeconds(pSec);
+        pAction();
+
+    }
+    
+
+    private void OnEnable()
+    {
+
+        if (playOnAwake)
+        {
+            if (!backGroundMusic)
+            {
+                //enable++;
+                //if (enable <= 1)
+                //{
+                //    return;
+                //}
+                if (delay == 0)
+                {
+                    SoundManager.Instance.PlaySound(audios[Random.Range(randomAudio.x,randomAudio.y)], Vector3.zero);
+                }
+                else
+                {
+                    StartCoroutine(delayAction(delay, delegate
+                    {
+                        SoundManager.Instance.PlaySound(audios[Random.Range(randomAudio.x, randomAudio.y)], Vector3.zero);
+                    }));
+                }
+            }
+            else
+            {
+                SoundManager.Instance.PlayBackgroundMusic(backGroundMusic);
+            }
+        }
+    }
+}
