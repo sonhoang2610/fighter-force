bd5e3ad565da0f349e07ddbd1b116b06e0894972
son_hoang_ <sonhoang2610@gmail.com>
2019-10-16 16:39:09 +0700

    asd

---
+++ Assets/SpaceJourney/Scripts/FireBaseInit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Analytics;
+
+namespace EazyEngine.Tools
+{
+ public class FireBaseInit : MonoBehaviour
+ {
+  // Start is called before the first frame update
+  void Start()
+  {
+   FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+   {
+    FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+   });
+  }
+ }
+}
+
