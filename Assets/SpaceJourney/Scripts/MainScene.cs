9088d84fe0d579c691652e60bb0f32ad9c87eb64
DESKTOP-IJKJH2P\Hoang Son <sonhoang2610@gmail.com>
2019-11-11 09:25:54 +0700

    asd

--- Assets/SpaceJourney/Scripts/MainScene.cs
+++ Assets/SpaceJourney/Scripts/MainScene.cs
@@ -207,30 +207,7 @@ namespace EazyEngine.Space.UI
 
         public void showBoxShop()
         {
-            public Vector3 SampleParabola(Vector3 start, Vector3 end, float height, float t)
-            {
-                float parabolicT = t * 2 - 1;
-                if (Mathf.Abs(start.y - end.y) < 0.1f)
-                {
-                    //start and end are roughly level, pretend they are - simpler solution with less steps
-                    Vector3 travelDirection = end - start;
-                    Vector3 result = start + t * travelDirection;
-                    result.y += (-parabolicT * parabolicT + 1) * height;
-                    return result;
-                }
-                else
-                {
-                    //start and end are not level, gets more complicated
-                    Vector3 travelDirection = end - start;
-                    Vector3 levelDirecteion = end - new Vector3(start.x, end.y, start.z);
-                    Vector3 right = Vector3.Cross(travelDirection, levelDirecteion);
-                    Vector3 up = Vector3.Cross(right, levelDirecteion);
-                    if (end.y > start.y) up = -up;
-                    Vector3 result = start + t * travelDirection;
-                    result += ((-parabolicT * parabolicT + 1) * height) * up.normalized;
-                    return result;
-                }
-            }
+        
             Firebase.Analytics.FirebaseAnalytics.LogEvent("PressShop");
             MidLayer.Instance.boxShop.show();
         }
