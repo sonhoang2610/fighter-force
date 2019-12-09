bdf704ba54c7d3fb69b70f0d07c4c57ddbc2fe5d
DESKTOP-IJKJH2P\Hoang Son <sonhoang2610@gmail.com>
2019-12-09 16:19:24 +0700

    fix

--- Assets/SpaceJourney/Scripts/SpaceJourneySupport.cs
+++ Assets/SpaceJourney/Scripts/SpaceJourneySupport.cs
@@ -6,6 +6,19 @@ namespace EazyEngine.Space
 {
     public static class SpaceJourneySupport
     {
+        public static int CountPassMode(int pMode)
+        {
+            var container = GameManager.Instance.container;
+            int pCount = 0;
+            for (int i = 0; i < container.levels.Count; ++i)
+            {
+                if (container.levels[i].isPassed && container.levels[i].hard == pMode)
+                {
+                    pCount++;
+                }
+            }
+            return pCount;
+        }
         public static LevelInfo CloneData(this LevelInfo pLevel)
         {
             var pInfo = new LevelInfo() {
