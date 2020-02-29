67bcb4e94fc7f0a3748ec040e983cb39fa01ccd0
DESKTOP-IJKJH2P\Hoang Son <sonhoang2610@gmail.com>
2020-02-29 17:59:00 +0700

    update

---
+++ Assets/SpaceJourney/Scripts/DatabaseNotifyReward.cs
@@ -0,0 +1,14 @@
+﻿using EazyEngine.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EazyEngine.Space
+{
+    [CreateAssetMenu(fileName = "DatabaseNotify",menuName = "EazyEngine/Space/DatabaseNotify")]
+    public class DatabaseNotifyReward : EzScriptTableObject
+    {
+        
+    }
+}
+
---
+++ Assets/SpaceJourney/Scripts/ItemNotifyReward.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EazyEngine.Space
+{
+    public class ItemNotifyReward : ItemPackage
+    {
+       
+    }
+}
